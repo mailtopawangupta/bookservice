0865bf90e786db7adf90b0197684d27c69c68610
pawan.gupta <pawan.gupta@fiserv.com>
2023-08-23 13:46:08 +0530

    Committed with SpringBoot

--- src/main/java/com/amazingbooks/bookservice/repository/Book.java
+++ src/main/java/com/amazingbooks/bookservice/repository/Book.java
@@ -1,11 +1,12 @@
 package com.amazingbooks.bookservice.repository;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+
 import java.io.Serializable;
 import java.sql.Date;
 
